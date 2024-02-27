b3a412744f3cdcdf22f5cee0cf7aba10191a1967
Mykyta Makhorin <nikitos2159@gmail.com>
2024-02-27 22:22:51 +0100

    update project.css and project.jsx

--- pages/Project.jsx
+++ pages/Project.jsx
@@ -1,7 +1,7 @@
 'use client'
 import '@styles/globals.css';
 import React from 'react';
-import '@styles/Main.css';
+import '@styles/Project.css';
 import '@styles/light_style.css';
 import Scroller from 'components/Scroller'
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
@@ -9,50 +9,72 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill, BsHouseAdd } from 'react-icons/bs';
 import { easeOut, motion as m } from "framer-motion"
 import NavbarM from '@components/NavbarMenu';
+import ProjectItems from 'components/ProjectItems'
+import Tello from 'public/assets/images/Tello.jpg'
+import Bagging_algoritm from 'public/assets/images/NN.png'
+import Unity from 'public/assets/images/Unity.gif'
 
 function Main() {
     return (
-        <m.div
-            initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: 0 }}
-            className="w-full h-screen flex justify-center items-center"
-            transition={{ duration: 0.1, ease: "easeOut" }}>
-            <div className = "navbar-container">
-                <NavbarM/>
-            </div>
-            <div className="flex flex-col md:flex-row justify-center items-center text-center max-w-[1100px] mx-auto">
-                <div>
-                    {/* Текстовый контент */}
-                    <p className='uppercase text-sm tracking-widest text-[#B7B7B7] text-center p-3'>123</p>
-
-                    {/* Для крупного текста используйте классы размера текста, подобранные под ваш дизайн */}
-                    <h1 className='text-6xl text-[#B1BDC9] text-center sm:hidden font-face-my69'>
-                        HI THERE, I'M <span className="text-[#d2d2d2]">123</span>
-                    </h1>
-                    {/* Текст для экранов начиная с sm и выше */}
-                    <h1 className='hidden sm:block lg:text-9xl text-[#B1BDC9] text-center font-face-my69'>
-                        HI THERE, I'M <span className=" key-tag whitespace-nowrap">213 123</span>
-                    </h1>
-                    {/* <h1 class="key-tag"><span>nIKITA</span></h1> */}
-
-                    {/* Для меньшего текста аналогично */}
-                    <h2 className='xs:text-2xl lg:text-4xl text-[#B1BDC9] text-center font-extralight'>
-                        Student of <span className="text-[#B7B7B7]">The Technical University of Košice </span>
-                        and a <span className="text-[#B7B7B7]">Junior</span> programmer.
-                    </h2>
-
-        
+        <>
+            <m.div
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                className="w-full h-screen flex text-left"
+                transition={{ duration: 0.1, ease: "easeOut" }}>
+                <div className="navbar-container">
+                    <NavbarM />
                 </div>
+                <div className="flex flex-col md:flex-row justify-start text-left w-full mx-auto xs:pt-32 md:pt-40  md:mr-96">
+                    <div>
+                        {/* Текстовый контент */}
+
+                        <p className='uppercase text-sm tracking-widest text-[#ffffffb7] text-left p-3 ml-14 '>Let's work together</p>
+
+                        <h1 className='text-6xl text-[#B1BDC9] text-center sm:hidden font-face-my69'>
+                            MY <span className="text-[#d2d2d2] whitespace-nowrap">PROJECTS.</span>
+                        </h1>
+
+                        <h1 className='hidden sm:block lg:text-9xl text-[#B1BDC9] font-face-my69 underline-full-width'>
+                            MY <span className=" key-tag whitespace-nowrap">PROJECTS.</span>
+                        </h1 >
+
+
+
+                    </div>
+                </div>
+
+            </m.div>
+
+
+            {/* Project element */}
+
+            <div className="w-full  min-height: 100vh">
+
+                <ProjectItems
+                    title_1='Drone-Face-Tracking-and-Photo-Classification-with-Tello'
+                    title_2='PyTorch'
+                    backgraundImg={Tello}
+                    projectUrl='https://github.com/Pudding2159/Drone-Face-Tracking-and-Photo-Classification-with-Tello'
+                />
+
+
+                <ProjectItems
+                    title_1='Simple Unity Game' 
+                    title_2='Welcome to my Unity game project'
+                    backgraundImg={Unity}
+                    projectUrl='https://github.com/Pudding2159/Unity-2D-Simple-Mobile-Game'
+                />
+
+                <ProjectItems
+                    title_1='Bagging Algorithm for Neural-Networks' s
+                    title_2='Sklearn'
+                    backgraundImg={Bagging_algoritm}
+                    projectUrl='https://github.com/Pudding2159/Bagging-Algorithm-for-Neural-Networks'
+                />
+
             </div>
-
-
-            <Scroller />
-            <script>
-                console.log(document.visibilityState);
-            </script>
-        </m.div>
-
-
+        </>
     );
 };
 
